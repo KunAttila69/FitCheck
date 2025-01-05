3193f5f2769ac239fec804282e4e88e7ef3f0427
KunAttila69 <klun.attila0508@gmail.com>
2025-01-05 16:29:55 +0100

    home phone base

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,17 @@
+import "./style.css"  
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <nav>
+        <div className="search">
+          <button></button>
+          <input type="text" placeholder="Search for user"/>
+        </div>
+        <div className="profile"></div>
+      </nav>
+      <main></main>
+    </>
   )
 }
 
