2bcf0e47c18d649f3a6f95437d231e4a69d00be7
KunAttila69 <klun.attila0508@gmail.com>
2025-01-10 10:40:43 +0100

    fix

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react"
 import "./style.css"  
+import { getUserProfile } from "../../services/authServices"
 
-const HomePage = () => {
+const HomePage = () => { 
+  const [profile, setProfile] = useState()
+  getUserProfile().then(res => {
+    setProfile(res)
+  })
+  
+  
   return (
     <>
-      <nav>
-        <div className="search">
-          <button></button>
-          <input type="text" placeholder="Search for user"/>
-        </div>
+      <nav>  
         <div className="profile"></div>
       </nav>
       <main></main>
+      <main>
+        <h1>Logged in as: {profile}</h1>
+      </main>
     </>
   )
 }
