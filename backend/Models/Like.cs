242ae23e046157ff29f310b8bbfd6938bcdd0a10
mskvszkyt <muskovszkyt@gmail.com>
2025-01-29 22:54:28 +0100

    created models for posting media on the app and added migration

---
+++ backend/Models/Like.cs
@@ -0,0 +1,17 @@
+﻿using FitCheck_Server.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+public class Like
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string UserId { get; set; }
+    [ForeignKey(nameof(UserId))]
+    public ApplicationUser User { get; set; }
+
+    public int PostId { get; set; }
+    public Post Post { get; set; }
+}
---
+++ backend/Models/PostMedia.cs
@@ -0,0 +1,21 @@
+﻿using FitCheck_Server.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class PostMedia
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string FilePath { get; set; } 
+    public MediaType Type { get; set; } 
+
+    public int PostId { get; set; }
+    public Post Post { get; set; }
+}
+
+public enum MediaType
+{
+    Image,
+    Video
+}
