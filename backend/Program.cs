061e61b3b2323cb36d13c7968eac9fa0ba9dff49
KunAttila69 <klun.attila0508@gmail.com>
2025-03-05 10:06:01 +0100

    add allow credentials

--- backend/Program.cs
+++ backend/Program.cs
@@ -60,10 +60,13 @@ builder.Services.AddScoped<FileService>();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "MyAllowSpecificOrigins",
-                      policy =>
-                      {
-                          policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin(); // TODO : Change this before release
-                      });
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+            .AllowCredentials()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
 });
 
 var app = builder.Build();
@@ -81,5 +84,5 @@ app.MapControllers();
 
 app.UseStaticFiles();
 
-app.UseCors();
+app.UseCors("MyAllowSpecificOrigins");
 app.Run();
