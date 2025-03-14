365db3fa06598c16b7f11e32a32f39e11074678c
KunAttila69 <klun.attila0508@gmail.com>
2025-03-14 13:55:59 +0100

    can upload multiple photos

--- frontend/src/pages/UploadPage/UploadPage.tsx
+++ frontend/src/pages/UploadPage/UploadPage.tsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 import styles from "./UploadPage.module.css";
 import Navbar from "../../components/Navbar/Navbar";
 
-const UploadPage: React.FC = () => {  
-    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+const UploadPage = () => {  
+    const [mediaFiles, setMediaFiles] = useState<string[]>([]);
  
-    const SelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (file) {
-            setSelectedImage(URL.createObjectURL(file)); 
+    const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        if (files) {
+            const newFiles = Array.from(files).map(file => URL.createObjectURL(file));
+            setMediaFiles(prevFiles => [...prevFiles, ...newFiles]); 
         }
+    };
+
+    const handleRemoveFile = (index: number) => {
+        setMediaFiles(prevFiles => prevFiles.filter((_, i) => i !== index));
     };
 
     return ( 
@@ -18,22 +23,34 @@ const UploadPage: React.FC = () => {
             <main className={styles.editContainer}>
                 <form className={styles.uploadForm}>
                     <label className={styles.imageContainer}>
-                        <input type="file" accept="image/*" onChange={SelectImage} style={{ display: "none" }} />
+                        <input type="file" accept="image/*,video/*" multiple onChange={handleFileSelect} style={{ display: "none" }} />
                         <div className={styles.uploadIcon}></div>
-                        {selectedImage ? (
-                            <img src={selectedImage} alt="Selected" className={styles.previewImage} />
-                        ) : (
-                            <h3>Select an image</h3>
-                        )}
+                        <h3>Select images or videos</h3>
                     </label>
+ 
+                    <div className={styles.mediaPreviewContainer}>
+                        {mediaFiles.map((file, i) => (
+                            <div key={i} className={styles.mediaPreview}>
+                                <div className={styles.removeUpload}><img src="../../src/images/delete.png" onClick={() => {handleRemoveFile(i)}}/></div>
+                                {file.includes("video") ? (
+                                    <video src={file} controls className={styles.previewVideo} />
+                                ) : (
+                                    <img src={file} alt={`Preview ${i}`} className={styles.previewImage} />
+                                )}
+                            </div>
+                        ))}
+                    </div>
+
                     <textarea 
                         className={styles.descriptionInput} 
                         placeholder="Add description"
                     ></textarea>
+
+                    <button className={styles.uploadButton}>Upload</button>
                 </form>
             </main>
         </>
     );
 };
- 
+
 export default UploadPage;
