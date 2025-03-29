5e9a6fbb2d9c8944ff5eabcb76337f3ba1f78a00
KunAttila69 <klun.attila0508@gmail.com>
2025-03-29 10:50:33 +0100

    added breakpoint to following

--- frontend/src/pages/FollowingPage/FollowingPage.tsx
+++ frontend/src/pages/FollowingPage/FollowingPage.tsx
@@ -3,6 +3,8 @@ import Navbar from "../../components/Navbar/Navbar";
 import Friend from "../../components/Friend/Friend";
 import { getFollowing } from "../../services/authServices";
 import Popup from "../../components/Popup/Popup";
+import LeaderboardComponent from "../../components/LeaderboardComponent/LeaderboardComponent";
+import styles from "./FollowingPage.module.css"
 
 interface FriendType {
   userId: string;
@@ -50,21 +52,26 @@ const FollowingPage = ({ profile }: PageProps) => {
     <>
       {message && (<Popup message={message.text} type={message.type} reset={() => {setMessage(null)}}/>)}
       <Navbar selectedPage="following" profilePic={profile.profilePictureUrl} />
-      <main>
-        {loading ? (
-          <p>Loading following...</p>
-        ) : following.length > 0 ? (
-          following.map((follow, i) => (
-            <Friend 
-              friend={follow} 
-              key={i} 
-              onUnfollow={() => handleUnfollow(follow.userId)} 
-              handleMessage={handleMessage}
-            />
-          ))
-        ) : (
-          <p>Looks like you don't follow anyone.</p>
-        )}
+      <main className={styles.followingPageMain}>
+        <div className={styles.leaderBoardContainer}>
+          <LeaderboardComponent/>
+        </div>
+        <div className={styles.followingContainer}>
+          {loading ? (
+            <p>Loading following...</p>
+          ) : following.length > 0 ? (
+            following.map((follow, i) => (
+              <Friend 
+                friend={follow} 
+                key={i} 
+                onUnfollow={() => handleUnfollow(follow.userId)} 
+                handleMessage={handleMessage}
+              />
+            ))
+          ) : (
+            <p>Looks like you don't follow anyone.</p>
+          )}
+        </div>
       </main>
     </>
   );
