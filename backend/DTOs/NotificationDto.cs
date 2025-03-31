cb62a94c7f4bca8d063b750cd20fbe30e996935d
muskovszky.tamas <muskovszky.tamas@BURG-1E06-05>
2025-03-31 10:44:11 +0200

    fix of possible null references

--- backend/DTOs/NotificationDto.cs
+++ backend/DTOs/NotificationDto.cs
@@ -6,11 +6,11 @@ namespace FitCheck_Server.DTOs
     public class NotificationDto
     {
         public int Id { get; set; }
-        public string ActorUsername { get; set; }
-        public string ActorProfilePictureUrl { get; set; }
+        public string? ActorUsername { get; set; }
+        public string? ActorProfilePictureUrl { get; set; }
         public NotificationType Type { get; set; }
         public int? PostId { get; set; }
-        public string PostThumbnailUrl { get; set; }
+        public string? PostThumbnailUrl { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool IsRead { get; set; }
     }
